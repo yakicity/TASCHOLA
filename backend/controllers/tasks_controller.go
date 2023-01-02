d04018fab2d0defe2368d0cdcd25a447fa13d5e2
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-02 22:29:45 +0900

    add create, update, delete controller

--- backend/controllers/tasks_controller.go
+++ backend/controllers/tasks_controller.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"net/http"
+	"strconv"
+	"taschola/db"
 	"taschola/models"
 
 	"github.com/gin-contrib/sessions"
@@ -50,4 +52,120 @@ func GetTasksByKeywordAndStatus(ctx *gin.Context) {
 	}
 
 	ctx.JSON(http.StatusOK, tasks)
+}
+
+func GetTask(ctx *gin.Context) {
+	// Get user ID from sessions
+	session := sessions.Default(ctx)
+	userID := session.Get("user_id")
+	if userID == nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	// Get task ID from request
+	taskID := ctx.Param("task_id")
+	taskIDUint64, err := strconv.ParseUint(taskID, 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid task ID"})
+		return
+	}
+
+	// Get task from database
+	task, err := models.GetTaskByUserIDAndTaskID(userID.(uint64), taskIDUint64)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, task)
+}
+
+func CreateTask(ctx *gin.Context) {
+	// Get user ID from sessions
+	session := sessions.Default(ctx)
+	userID := session.Get("user_id")
+	if userID == nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	// Get task from request
+	var task db.Task
+	err := ctx.BindJSON(&task)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid task"})
+		return
+	}
+
+	// Create task in database
+	taskID, err := models.CreateTask(task, userID.(uint64))
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"task_id": taskID})
+}
+
+func UpdateTask(ctx *gin.Context) {
+	// Get user ID from sessions
+	session := sessions.Default(ctx)
+	userID := session.Get("user_id")
+	if userID == nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	// Get task ID from request
+	taskID := ctx.Param("task_id")
+	taskIDUint64, err := strconv.ParseUint(taskID, 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid task ID"})
+		return
+	}
+
+	// Get task from request
+	var task db.Task
+	err = ctx.BindJSON(&task)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid task"})
+		return
+	}
+
+	// Update task in database
+	err = models.UpdateTaskByUserIDAndTaskID(task, userID.(uint64), taskIDUint64)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"task_id": taskID})
+}
+
+func DeleteTask(ctx *gin.Context) {
+	// Get user ID from sessions
+	session := sessions.Default(ctx)
+	userID := session.Get("user_id")
+	if userID == nil {
+		ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+		return
+	}
+
+	// Get task ID from request
+	taskID := ctx.Param("task_id")
+	taskIDUint64, err := strconv.ParseUint(taskID, 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "invalid task ID"})
+		return
+	}
+
+	// Delete task from database
+	err = models.DeleteTaskByUserIDAndTaskID(userID.(uint64), taskIDUint64)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"task_id": taskID})
 }
