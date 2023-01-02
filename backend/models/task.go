b72cea03fbd28f389fc064c6cca69ec569ce426b
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-02 21:32:21 +0900

    add create, update, delete func

--- backend/models/task.go
+++ backend/models/task.go
@@ -48,3 +48,70 @@ func GetTaskByTaskID(taskID int) (database.Task, error) {
 
 	return task, nil
 }
+
+func CreateTask(task database.Task, userID uint64) (int, error) {
+	db, err := database.GetConnection()
+	if err != nil {
+		return 0, err
+	}
+
+	tx, err := db.Beginx()
+	if err != nil {
+		return 0, err
+	}
+
+	err = tx.Get(&task, "INSERT INTO tasks (title, description, status, priority, due_date) VALUES (?, ?, ?, ?, ?)", task.Title, task.Description, task.Status, task.Priority, task.DueDate)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	var taskID int
+	err = tx.Get(&taskID, "SELECT LAST_INSERT_ID()")
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	_, err = tx.Exec("INSERT INTO ownerships (user_id, task_id) VALUES (?, ?)", userID, taskID)
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return 0, err
+	}
+
+	return taskID, nil
+}
+
+func UpdateTask(task database.Task) error {
+	db, err := database.GetConnection()
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec("UPDATE tasks SET title = ?, description = ?, status = ?, priority = ?, due_date = ? WHERE id = ?", task.Title, task.Description, task.Status, task.Priority, task.DueDate, task.ID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func DeleteTask(taskID int) error {
+	db, err := database.GetConnection()
+	if err != nil {
+		return err
+	}
+
+	// delete task from tasks table and ownerships table
+	_, err = db.Exec("DELETE tasks, ownerships FROM tasks INNER JOIN ownerships ON tasks.id = ownerships.task_id WHERE tasks.id = ?", taskID)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
