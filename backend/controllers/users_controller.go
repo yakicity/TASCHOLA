b5e04b509076b8e1825e83869e0b278c9b0f339e
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-07 00:12:54 +0900

    fix user controller

--- backend/controllers/users_controller.go
+++ backend/controllers/users_controller.go
@@ -6,12 +6,11 @@ import (
 	"taschola/db"
 	"taschola/models"
 
-	"github.com/gin-contrib/sessions"
 	"github.com/gin-gonic/gin"
 )
 
-//newuser
-//POST"v1/user/new"
+// new user
+// POST v1/user/new
 //
 // @Param user [database.User]
 //
@@ -19,12 +18,12 @@ import (
 // @Failure 400 {"error": "Bad Request (invalid user)"}
 // @Failure 409 {"error": "Conflict (duplicate user name)"}
 // @Failure 500 {"error": "Internal Server Error"}
-func CreateUser(ctx *gin.Context){
+func CreateUser(ctx *gin.Context) {
 	// get user
 	var user db.User
 	err := ctx.BindJSON(&user)
 	if err != nil {
-		ctx.String(http.StatusBadRequest, gin.H{"error": "Bad Request (invalid user)"})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (invalid user)"})
 		return
 	}
 
@@ -32,34 +31,31 @@ func CreateUser(ctx *gin.Context){
 	switch models.CheckDuplicateUserName(user.Name) {
 	case true: // duplicate
 		ctx.JSON(http.StatusConflict, gin.H{"error": "Conflict (duplicate user name)"})
-		
 		return
+
 	default: // not duplicate
 		// create user
-		err = models.CreateUser(&user)
+		err := models.CreateUser(user)
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"Internal Server Error"})
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
 			return
 		}
 
-		ctx.JSON(http.StatusOK, gin.H{
-			"user": user,
-		})
+		ctx.JSON(http.StatusOK, gin.H{"status": "success"})
 	}
 }
 
-
-//GET"v1/user/:id"
+// GET v1/user/:id
 //
 // @Param id [uint64]
 // @Param user [database.User]
 //
 // @Success 200 { "user": database.User}
-// @Failure 404 
-func GetUser(ctx *gin.Context){
-	userID, err := strconv.Atoi(ctx.Param("id"))
+// @Failure 404
+func GetUser(ctx *gin.Context) {
+	userID, err := strconv.ParseUint((ctx.Param("id")), 10, 64)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"Bad Request (invalid user id)"})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (invalid user id)"})
 		return
 	}
 
@@ -74,57 +70,64 @@ func GetUser(ctx *gin.Context){
 	})
 }
 
-
-//PUT"v1/user/:id"
-//
-// @Param id [uint64]
-//
-// @Success 200 { "user_id": uint64}
-// @Failure 400 { "error": "invalid user id"}
-// @Failure 401 { "error": "Internal Server Error"} 
-// @Failure 409 {"error": "Conflict (duplicate user name)"}
-func EditUser(ctx *gin.Context){
-	userID, err := strconv.Atoi(ctx.Param("id"))
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"Bad Request (invalid user id)"})
-		return
-	}
-	// get user
-	var user db.User
-	err := ctx.BindJSON(&user)
-	if err != nil {
-		ctx.String(http.StatusBadRequest, gin.H{"error": "Bad Request (invalid user)"})
-		return
-	}
-
-	//重複判定
-	if (models.CheckDuplicateUserName(user.Name)){
-		ctx.JSON(http.StatusConflict, gin.H{"error": "Conflict (duplicate user name)"})
-	}
-
-	//update user information
-	err = models.UpdateUser(user)
-	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, "Internal Server Error")
-		return
-	}
-
-	ctx.JSON(http.StatusOK, gin.H{"user_id": userID})
+type UserForm struct {
+	ConfirmPassword string  `json:"confirm_password"`
+	User            db.User `json:"user"`
 }
 
-//userdelete
-//DELETE"v1/user/:id"
+// PUT v1/user/:id
 //
 // @Param id [uint64]
 //
 // @Success 200 { "user_id": uint64}
 // @Failure 400 { "error": "invalid user id"}
 // @Failure 401 { "error": "Internal Server Error"}
-func DeleteUser(ctx *gin.Context){
-	// get user_id
-	userID, err := strconv.Atoi(ctx.Param("id"))
+// @Failure 409 {"error": "Conflict (duplicate user name)"}
+func EditUser(ctx *gin.Context) {
+	userID, err := strconv.ParseUint((ctx.Param("id")), 10, 64)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"Bad Request (invalid user id)"})
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (invalid user id)"})
+		return
+	}
+
+	var userForm UserForm
+	err = ctx.BindJSON(&userForm)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (invalid user)"})
+		return
+	}
+
+	// check duplicate user name
+	switch models.CheckDuplicateUserName(userForm.User.Name) {
+	case true: // duplicate
+		ctx.JSON(http.StatusConflict, gin.H{"error": "Conflict (duplicate user name)"})
+		return
+
+	default: // not duplicate
+		// update user
+		err := models.UpdateUser(userID, userForm.User)
+		if err != nil {
+			ctx.String(http.StatusInternalServerError, "Internal Server Error")
+			return
+		}
+
+		ctx.JSON(http.StatusOK, gin.H{"user_id": userID})
+	}
+}
+
+// user delete
+// DELETE v1/user/:id
+//
+// @Param id [uint64]
+//
+// @Success 200 { "user_id": uint64}
+// @Failure 400 { "error": "invalid user id"}
+// @Failure 401 { "error": "Internal Server Error"}
+func DeleteUser(ctx *gin.Context) {
+	// get user_id
+	userID, err := strconv.ParseUint((ctx.Param("id")), 10, 64)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (invalid user id)"})
 		return
 	}
 
