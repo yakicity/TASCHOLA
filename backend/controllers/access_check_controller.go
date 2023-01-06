a0a335ca25e83487be297fe655e2d82f99789a4d
brown96 <daichees@icloud.com>
2023-01-07 01:05:21 +0900

    add tentative LoginCheck

--- backend/controllers/access_check_controller.go
+++ backend/controllers/access_check_controller.go
@@ -1,3 +1,22 @@
 package controllers
 
-//Logincheck
+import (
+	"net/http"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
+
+const userkey = "user"
+const frontEndURL string = "http://localhost:3000"
+
+// Logincheck
+func LoginCheck(ctx *gin.Context) {
+	userID := sessions.Default(ctx).Get(userkey)
+	if userID == nil {
+		ctx.Redirect(http.StatusFound, frontEndURL+"/login")
+		ctx.Abort()
+	} else {
+		ctx.Next()
+	}
+}
