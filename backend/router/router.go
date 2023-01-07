785d6f737fc112efd953893ee5c948baf0490b19
brown96 <86361503+brown96@users.noreply.github.com>
2023-01-08 01:06:08 +0900

    apply changes to router

--- backend/router/router.go
+++ backend/router/router.go
@@ -32,23 +32,34 @@ func Init() *gin.Engine {
 		v1.GET("/", controllers.NotImplemented)
 
 		// tasks
-		v1.GET("/tasks", controllers.NotImplemented)
-		v1.GET("/tasks/t2schola_sync", controllers.NotImplemented)
-
-		v1.GET("/task/:id", controllers.NotImplemented)
-		v1.PUT("/task/:id", controllers.NotImplemented)
-		v1.DELETE("/task/:id", controllers.NotImplemented)
-		v1.POST("/task/new", controllers.NotImplemented)
+		tasks := v1.Group("/tasks")
+		tasks.Use(controllers.LoginCheck)
+		{
+			tasks.GET("/", controllers.GetTasks)
+			tasks.GET("/t2schola_sync", controllers.NotImplemented)
+		}
+		task := v1.Group("/task")
+		task.Use(controllers.LoginCheck)
+		{
+			task.GET("/:id", controllers.GetTask)
+			task.PUT("/:id", controllers.UpdateTask)
+			task.DELETE("/:id", controllers.DeleteTask)
+			task.POST("/new", controllers.CreateTask)
+		}
 
 		// auth
-		v1.POST("/login", controllers.NotImplemented)
-		v1.POST("/logout", controllers.NotImplemented)
+		v1.POST("/login", controllers.Login)
+		v1.POST("/logout", controllers.Logout)
 
 		// user
-		v1.POST("/user/new", controllers.NotImplemented)
-		v1.GET("/user/:id", controllers.NotImplemented)
-		v1.PUT("/user/:id", controllers.NotImplemented)    // edit
-		v1.DELETE("/user/:id", controllers.NotImplemented) // delete
+		v1.POST("/user/new", controllers.CreateUser)
+		user := v1.Group("/user")
+		user.Use(controllers.LoginCheck)
+		{
+			v1.GET("/:id", controllers.GetUser)
+			v1.PUT("/:id", controllers.UpdateUser)    // edit
+			v1.DELETE("/:id", controllers.DeleteUser) // delete
+		}
 	}
 
 	return engine
