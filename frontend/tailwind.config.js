2bd2414d0820e13e57818a48f797cc88358ed3b7
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-07 11:34:14 +0900

    add tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purge: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
