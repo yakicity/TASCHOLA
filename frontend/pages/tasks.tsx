b4448ddee288ca38ac8eba3e1692ec92e4af9fd8
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-10 00:28:47 +0900

    add create task button

--- frontend/pages/tasks.tsx
+++ frontend/pages/tasks.tsx
@@ -28,11 +28,15 @@ const TasksPage = () => {
     <>
       <main className={styles.main}>
         <div>
-          <h1>Task List</h1>
-          <a href="/task/new" className="btn btn-primary">
-            Create Task
-          </a>
-          <TaskList tasks={taskList} />
+          <div>
+            <a href="/task/new" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+              Create a new Task
+            </a>
+          </div>
+
+          <div>
+            <TaskList tasks={taskList} />
+          </div>
         </div>
       </main>
     </>
