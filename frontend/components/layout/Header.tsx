ed600195e711bec7eeda053f9a051bbd7d04a94d
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-14 16:17:00 +0900

    change a tag -> next Link

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 const Header = () => {
   return (
     <header>
@@ -5,21 +7,21 @@ const Header = () => {
         <div className="mx-auto max-w-7xl px-6">
           <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
             <div className="flex justify-start lg:w-0 lg:flex-1">
-              <a href="/">
+              <Link href="/">
                 <span className="sr-only">Your Company</span>
                 <img className="h-8 w-auto sm:h-10" src="/taschola.svg" alt="" />
-              </a>
+              </Link>
             </div>
 
             <nav className="hidden space-x-10 md:flex">
-              <a href="/tasks" className="text-base font-medium text-gray-500 hover:text-gray-900">Tasks</a>
-              <a href="/user" className="text-base font-medium text-gray-500 hover:text-gray-900">Profile</a>
-              <a href="" className="text-base font-medium text-gray-500 hover:text-gray-900">Sync</a>
+              <Link href="/tasks" className="text-base font-medium text-gray-500 hover:text-gray-900">Tasks</Link>
+              <Link href="/user" className="text-base font-medium text-gray-500 hover:text-gray-900">Profile</Link>
+              <Link href="" className="text-base font-medium text-gray-500 hover:text-gray-900">Sync</Link>
             </nav>
 
             <div className="hidden items-center justify-end md:flex md:flex-1 lg:w-0">
-              <a href="/login" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Login</a>
-              <a href="/signup" className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign up</a>
+              <Link href="/login" className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Login</Link>
+              <Link href="/signup" className="ml-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700">Sign up</Link>
             </div>
           </div>
         </div>
