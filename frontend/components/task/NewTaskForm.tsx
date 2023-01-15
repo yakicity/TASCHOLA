5574fa2791c375ef6a0cdd05a6e636a13c5b9b3f
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-15 22:21:37 +0900

    add axios api request to task/new

--- frontend/components/task/NewTaskForm.tsx
+++ frontend/components/task/NewTaskForm.tsx
@@ -1,4 +1,29 @@
+import axios from 'axios'
+import { useState } from 'react'
+
 const NewTaskForm = () => {
+  const [title, setTitle] = useState<string>("")
+  const [description, setDescription] = useState<string>("")
+  const [status, setStatus] = useState<string>("TODO")
+  const [priority, setPriority] = useState<number>(1)
+  const [dueDate, setDueDate] = useState<string>("")
+
+  const handleSubmit = () => {
+    const data = {
+      title: title,
+      description: description,
+      status: status,
+      priority: priority,
+      due_date: dueDate,
+    }
+
+    axios.post(`localhost:8000/v1/task`, data)
+      .then((res) => {
+        console.log(res)
+      }
+      )
+  }
+
   return (
     <div className="mt-10 sm:mt-0">
       <div className="md:grid md:grid-cols-3 md:gap-6">
@@ -15,42 +40,42 @@ const NewTaskForm = () => {
                 <div className="grid grid-cols-6 gap-6">
                   <div className="col-span-6 sm:col-span-4">
                     <label htmlFor="user_name" className="block text-sm font-medium text-gray-700">Title</label>
-                    <input type="text" name="user_name" id="user_name" autoComplete="user_name" className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+                    <input type="text" className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" value={title} onChange={(event) => setTitle(event?.target.value)} />
                   </div>
 
                   <div className="col-span-6 sm:col-span-4">
-                    <label htmlFor="password1" className="block text-sm font-medium text-gray-700">Description</label>
-                    <input type="text" name="password1" id="password1" autoComplete="password" className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-20" />
+                    <label htmlFor="description" className="block text-sm font-medium text-gray-700">Description</label>
+                    <input type="text" className="mt-1 block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm h-20" value={description} onChange={(event) => setDescription(event?.target.value)} />
                   </div>
 
                   <div className="col-span-6 sm:col-span-4">
-                    <label htmlFor="password2" className="block text-sm font-medium text-gray-700">Status</label>
-                    <select id="student_type" name="student_type" autoComplete="degree" className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                      <option>TODO</option>
-                      <option>DOING</option>
-                      <option>DONE</option>
+                    <label htmlFor="status" className="block text-sm font-medium text-gray-700">Status</label>
+                    <select className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" value={status} onChange={(event) => setStatus(event?.target.value)}>
+                      <option value="TODO">TODO</option>
+                      <option value="DOING">DOING</option>
+                      <option value="DONE">DONE</option>
                     </select>
                   </div>
 
                   <div className="col-span-6 sm:col-span-4">
-                    <label htmlFor="student_type" className="block text-sm font-medium text-gray-700">Priority</label>
-                    <select id="student_type" name="student_type" autoComplete="degree" className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
-                      <option>1 (Highest)</option>
-                      <option>2 (High)</option>
-                      <option>3 (Middle)</option>
-                      <option>4 (Low)</option>
-                      <option>5 (Lowest)</option>
+                    <label htmlFor="priority" className="block text-sm font-medium text-gray-700">Priority</label>
+                    <select className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm" value={priority} onChange={(event) => setPriority(Number(event?.target.value))}>
+                      <option value="1">1 (Highest)</option>
+                      <option value="2">2 (High)</option>
+                      <option value="3">3 (Middle)</option>
+                      <option value="4">4 (Low)</option>
+                      <option value="5">5 (Lowest)</option>
                     </select>
                   </div>
 
                   <div className="col-span-6 sm:col-span-4">
-                    <label htmlFor="student_type" className="block text-sm font-medium text-gray-700">Due Date</label>
-                    <input type="date" name="student_type" id="student_type" autoComplete="degree" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+                    <label htmlFor="due_date" className="block text-sm font-medium text-gray-700">Due Date</label>
+                    <input type="date" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" value={dueDate} onChange={(event) => setDueDate(event?.target.value)} />
                   </div>
                 </div>
               </div>
               <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                <button type="submit" className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Create</button>
+                <button type="submit" className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2" onClick={handleSubmit}>Create</button>
               </div>
             </div>
           </form>
