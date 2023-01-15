ec5110753707d3b5176fb77f624d4e1e13543e7e
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-15 22:06:36 +0900

    add axios get request task/:id

--- frontend/pages/task/[id]/index.tsx
+++ frontend/pages/task/[id]/index.tsx
@@ -1,26 +1,34 @@
 import DetailTask from '@/components/task/DetailTask'
 import { Task } from '@/interfaces/task'
 import styles from '@/styles/Home.module.scss'
+import axios from 'axios'
 import { useRouter } from 'next/router'
-
-const task = {
-  id: 1,
-  title: 'Task 1',
-  description: 'Task 1 description test test test test .....................test test sentence',
-  status: 'TODO',
-  created_at: '2020-01-01',
-  priority: 1,
-  due_date: '2021-01-01',
-} as Task
+import { useEffect, useState } from 'react'
 
 const TaskPage = () => {
   const router = useRouter()
   const { id } = router.query
 
+  const [task, setTask] = useState<Task>()
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    (async () => {
+      await axios.get(`http://localhost:8000/v1/task/${id}`)
+        .then((res) => {
+          setTask(res.data)
+          setLoading(false)
+        }
+        )
+    })()
+  }, [])
+
   return (
     <>
       <main className={styles.main}>
-        <DetailTask task={task} />
+        {loading && <div>Loading...</div>}
+        {!loading && !task && <div>No task found</div>}
+        {!loading && task && <DetailTask task={task} />}
       </main>
     </>
   )
