28b8f22d196c37463b0467d5b161d9558e948b8c
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-17 23:17:39 +0900

    add swagger annotation to user controller

--- backend/controllers/users_controller.go
+++ backend/controllers/users_controller.go
@@ -19,46 +19,66 @@ type UpdateUserForm struct {
 	User        UserForm `json:"user"`
 }
 
-// GET v1/user/:id
+// GetUser
 //
-// @Param id [uint64]
-// @Param user [database.User]
-//
-// @Success 200 { "user": database.User}
-// @Failure 404
+//	@Summary	Get User
+//	@Tags		user
+//	@Accept		json
+//	@Produce	json
+//	@Param		id	path		uint64	true	"user id"
+//	@Success	200	{object}	db.User
+//	@Failure	400	{object}	models.HTTPError
+//	@Failure	404	{object}	models.HTTPError
+//	@Router		/v1/user/{id} [get]
 func GetUser(ctx *gin.Context) {
 	userID, err := strconv.ParseUint((ctx.Param("id")), 10, 64)
+
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (invalid user id)"})
+		httpError := models.HTTPError{
+			Code:  http.StatusBadRequest,
+			Error: "Bad Request (invalid user id)",
+			Place: "controllers.GetUser",
+		}
+		ctx.JSON(http.StatusBadRequest, httpError)
 		return
 	}
 
 	user, err := models.GetUserByID(userID)
 	if err != nil {
-		ctx.AbortWithStatus(http.StatusNotFound)
+		httpError := models.HTTPError{
+			Code:  http.StatusNotFound,
+			Error: "Not Found (such a user id does not exist)",
+			Place: "controllers.GetUser",
+		}
+		ctx.JSON(http.StatusNotFound, httpError)
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{
-		"user": user,
-	})
+	ctx.JSON(http.StatusOK, user)
 }
 
-// POST v1/user/new
+// CreateUser
 //
-// @Param user [database.User]
-//
-// @Success 200 {"user": user}
-// @Failure 400 {"error": "Bad Request (invalid user)"}
-// @Failure 409 {"error": "Conflict (duplicate user name)"}
-// @Failure 500 {"error": "Internal Server Error"}
-
+//	@Summary	Create User
+//	@Tags		user
+//	@Accept		json
+//	@Produce	json
+//	@Success	200
+//	@Failure	400	{object}	models.HTTPError
+//	@Failure	409	{object}	models.HTTPError
+//	@Failure	500	{object}	models.HTTPError
+//	@Router		/v1/user [post]
 func CreateUser(ctx *gin.Context) {
 	// get user
 	var userInfo UserForm
 	err := ctx.BindJSON(&userInfo)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (userForm is invalid)"})
+		httpError := models.HTTPError{
+			Code:  http.StatusBadRequest,
+			Error: "Bad Request (userForm is invalid)",
+			Place: "controllers.CreateUser",
+		}
+		ctx.JSON(http.StatusBadRequest, httpError)
 		return
 	}
 
@@ -66,14 +86,24 @@ func CreateUser(ctx *gin.Context) {
 	// check duplicate user name
 	switch models.CheckDuplicateUserName(user.Name) {
 	case true: // duplicate
-		ctx.JSON(http.StatusConflict, gin.H{"error": "Conflict (duplicate user name)"})
+		httpError := models.HTTPError{
+			Code:  http.StatusConflict,
+			Error: "Conflict (duplicate user name)",
+			Place: "controllers.CreateUser",
+		}
+		ctx.JSON(http.StatusConflict, httpError)
 		return
 
 	default: // not duplicate
 		// create user
 		err := models.CreateUser(user)
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
+			httpError := models.HTTPError{
+				Code:  http.StatusInternalServerError,
+				Error: "Internal Server Error: " + err.Error(),
+				Place: "controllers.CreateUser",
+			}
+			ctx.JSON(http.StatusInternalServerError, httpError)
 			return
 		}
 
@@ -81,43 +111,74 @@ func CreateUser(ctx *gin.Context) {
 	}
 }
 
-// PUT v1/user/:id
+// UpdateUser
 //
-// @Param id [uint64]
-//
-// @Success 200 { "user_id": uint64}
-// @Failure 400 { "error": "invalid user id"}
-// @Failure 401 { "error": "Internal Server Error"}
-// @Failure 409 {"error": "Conflict (duplicate user name)"}
+//	@Summary	Update User
+//	@Tags		user
+//	@Accept		json
+//	@Produce	json
+//	@Param		id		path		uint64			true	"user id"
+//	@Param		user	body		UpdateUserForm	true	"user"
+//	@Success	200		integer		user			ID
+//	@Failure	400		{object}	models.HTTPError
+//	@Failure	401		{object}	models.HTTPError
+//	@Failure	409		{object}	models.HTTPError
+//	@Failure	500		{object}	models.HTTPError
+//	@Router		/v1/user/{id} [put]
 func UpdateUser(ctx *gin.Context) {
 	userID, err := strconv.ParseUint((ctx.Param("id")), 10, 64)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (invalid user id)"})
+		httpError := models.HTTPError{
+			Code:  http.StatusBadRequest,
+			Error: "Bad Request (invalid user id)",
+			Place: "controllers.UpdateUser",
+		}
+		ctx.JSON(http.StatusBadRequest, httpError)
 		return
 	}
 
 	var userForm UpdateUserForm
 	err = ctx.BindJSON(&userForm)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (updateUserForm is invalid)"})
+		httpError := models.HTTPError{
+			Code:  http.StatusBadRequest,
+			Error: "Bad Request (updateUserForm is invalid)",
+			Place: "controllers.UpdateUser",
+		}
+		ctx.JSON(http.StatusBadRequest, httpError)
 		return
 	}
 
 	// check old password
 	oldUserInfo, err := models.GetUserByID(userID)
 	if err != nil {
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "The user is not found"})
+		httpError := models.HTTPError{
+			Code:  http.StatusInternalServerError,
+			Error: "Not Found (such a user id does not exist)",
+			Place: "controllers.UpdateUser",
+		}
+		ctx.JSON(http.StatusInternalServerError, httpError)
 		return
 	}
 	if oldUserInfo.Password == nil || string(oldUserInfo.Password) != string(hash(userForm.OldPassword)) {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (old password is wrong)"})
+		httpError := models.HTTPError{
+			Code:  http.StatusBadRequest,
+			Error: "Bad Request (old password is wrong)",
+			Place: "controllers.UpdateUser",
+		}
+		ctx.JSON(http.StatusBadRequest, httpError)
 		return
 	}
 
 	// check duplicate user name
 	switch models.CheckDuplicateUserName(userForm.User.Name) {
 	case true: // duplicate
-		ctx.JSON(http.StatusConflict, gin.H{"error": "Conflict (duplicate user name)"})
+		httpError := models.HTTPError{
+			Code:  http.StatusConflict,
+			Error: "Conflict (duplicate user name)",
+			Place: "controllers.UpdateUser",
+		}
+		ctx.JSON(http.StatusConflict, httpError)
 		return
 
 	default: // not duplicate
@@ -125,37 +186,56 @@ func UpdateUser(ctx *gin.Context) {
 		user := encryptUserInfo(userForm.User)
 		err := models.UpdateUser(userID, user)
 		if err != nil {
-			ctx.String(http.StatusInternalServerError, "Internal Server Error")
+			httpError := models.HTTPError{
+				Code:  http.StatusInternalServerError,
+				Error: "Internal Server Error: " + err.Error(),
+				Place: "controllers.UpdateUser",
+			}
+			ctx.JSON(http.StatusInternalServerError, httpError)
 			return
 		}
 
-		ctx.JSON(http.StatusOK, gin.H{"user_id": userID})
+		ctx.JSON(http.StatusOK, userID)
 	}
 }
 
-// DELETE v1/user/:id
+// DeleteUser
 //
-// @Param id [uint64]
-//
-// @Success 200 { "user_id": uint64}
-// @Failure 400 { "error": "invalid user id"}
-// @Failure 401 { "error": "Internal Server Error"}
+//	@Summary	Delete User
+//	@Tags		user
+//	@Accept		json
+//	@Produce	json
+//	@Param		id	path		uint64	true	"user id"
+//	@Success	200	integer		user	ID
+//	@Failure	400	{object}	models.HTTPError
+//	@Failure	500	{object}	models.HTTPError
+//	@Router		/v1/user/{id} [delete]
 func DeleteUser(ctx *gin.Context) {
 	// get user_id
 	userID, err := strconv.ParseUint((ctx.Param("id")), 10, 64)
 	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": "Bad Request (invalid user id)"})
+		httpError := models.HTTPError{
+			Code:  http.StatusBadRequest,
+			Error: "Bad Request (invalid user id)",
+			Place: "controllers.DeleteUser",
+		}
+		ctx.JSON(http.StatusBadRequest, httpError)
 		return
 	}
 
 	// delete user
 	err = models.DeleteUser(userID)
 	if err != nil {
-		ctx.String(http.StatusInternalServerError, "Internal Server Error")
+		httpError := models.HTTPError{
+			Code:  http.StatusInternalServerError,
+			Error: "Internal Server Error: " + err.Error(),
+			Place: "controllers.DeleteUser",
+		}
+		ctx.JSON(http.StatusInternalServerError, httpError)
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"user_id": userID})
+	ctx.JSON(http.StatusOK, userID)
 }
 
 // private functions
