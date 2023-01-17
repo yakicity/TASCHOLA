e5941e32ec111ef585656f37e090a6902c8a8302
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-17 13:28:40 +0900

    add swagger annotation

--- backend/controllers/default_controller.go
+++ backend/controllers/default_controller.go
@@ -7,12 +7,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// NotImplemented
+//	@Summary		Not implemented
+//	@Description	for not implemented methods
+//	@Tags			default
+//	@Accept			json
+//	@Produce		json
+//	@Success		501	{object}	models.Error
+//	@Router			/ [get]
 func NotImplemented(ctx *gin.Context) {
 	msg := fmt.Sprintf("%s access to %s is not implemented yet", ctx.Request.Method, ctx.Request.URL)
 	ctx.Header("Cache-Control", "no-cache")
 	ctx.String(http.StatusNotImplemented, msg)
 }
 
+// HealthCheck
+//	@Summary	Health check
+// 	@Description	Health check (for Docker healthcheck)
+//	@Tags		default
+//	@Accept		json
+//	@Produce	json
+//	@Success	200	{string}	string	"OK"
+//	@Router		/health [get]
 func HealthCheck(ctx *gin.Context) {
 	ctx.Header("Cache-Control", "no-cache")
 	ctx.String(http.StatusOK, "OK")
