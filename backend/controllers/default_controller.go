7a194672d43192daf1d1ff056c82bd0d88cdbf82
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-17 22:32:54 +0900

    add swagger annotation

--- backend/controllers/default_controller.go
+++ backend/controllers/default_controller.go
@@ -4,31 +4,40 @@ import (
 	"fmt"
 	"net/http"
 	database "taschola/db"
+	"taschola/models"
 
 	"github.com/gin-gonic/gin"
 )
 
 // NotImplemented
+//
 //	@Summary		Not implemented
 //	@Description	for not implemented methods
 //	@Tags			default
 //	@Accept			json
 //	@Produce		json
-//	@Success		501	{object}	models.Error
+//	@Success		501	{object}	models.HTTPError
 //	@Router			/ [get]
 func NotImplemented(ctx *gin.Context) {
 	msg := fmt.Sprintf("%s access to %s is not implemented yet", ctx.Request.Method, ctx.Request.URL)
+	err := models.HTTPError{
+		Code:  http.StatusNotImplemented,
+		Error: msg,
+		Place: "NotImplemented",
+	}
+
 	ctx.Header("Cache-Control", "no-cache")
-	ctx.String(http.StatusNotImplemented, msg)
+	ctx.JSON(http.StatusNotImplemented, err)
 }
 
 // HealthCheck
+//
 //	@Summary	Health check
-// 	@Description	Health check (for Docker healthcheck)
+//	@Description	Health check (for Docker health check)
 //	@Tags		default
 //	@Accept		json
 //	@Produce	json
-//	@Success	200	{string}	string	"OK"
+//	@Success	200	{string} OK "Health check OK"
 //	@Router		/health [get]
 func HealthCheck(ctx *gin.Context) {
 	ctx.Header("Cache-Control", "no-cache")
@@ -36,33 +45,49 @@ func HealthCheck(ctx *gin.Context) {
 }
 
 // CheckDBConnection
+//
 //	@Summary	Check DB connection
-// 	@Description	Check DB connection
+//	@Description	Check DB connection
 //	@Tags		default
 //	@Accept		json
 //	@Produce	json
-//	@Success	200	{string}	string	"OK"
-//	@Failure	500	{object}	models.Error
-//	@Router		/heathz [get]
+//	@Success	200	{array}	string	"OK"
+//	@Failure	500	{object}	models.HTTPError
+//	@Router		/healthz [get]
 func CheckDBConnection(ctx *gin.Context) {
 	db, err := database.GetConnection()
 	if err != nil {
 		ctx.Header("Cache-Control", "no-cache")
-		ctx.String(http.StatusInternalServerError, "DB connection error")
+		errResponse := models.HTTPError{
+			Code:  http.StatusInternalServerError,
+			Error: "DB connection error: " + err.Error(),
+			Place: "CheckDBConnection",
+		}
+		ctx.JSON(http.StatusInternalServerError, errResponse.Error)
 		return
 	}
 	err = db.Ping()
 	if err != nil {
+		errResponse := models.HTTPError{
+			Code:  http.StatusInternalServerError,
+			Error: "DB connection error: " + err.Error(),
+			Place: "CheckDBConnection",
+		}
 		ctx.Header("Cache-Control", "no-cache")
-		ctx.String(http.StatusInternalServerError, "DB connection error")
+		ctx.JSON(http.StatusInternalServerError, errResponse.Error)
 		return
 	}
 
 	var tableNames []string
 	err = db.Select(&tableNames, "SHOW TABLES")
 	if err != nil {
+		errResponse := models.HTTPError{
+			Code:  http.StatusInternalServerError,
+			Error: "DB connection error: " + err.Error(),
+			Place: "CheckDBConnection",
+		}
 		ctx.Header("Cache-Control", "no-cache")
-		ctx.String(http.StatusInternalServerError, "DB connection error")
+		ctx.JSON(http.StatusInternalServerError, errResponse.Error)
 		return
 	}
 
