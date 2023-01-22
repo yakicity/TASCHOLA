eb1bb1b06086ed91d495c5ecf71f089b3c0570b7
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-22 20:52:37 +0900

    change jsx

--- frontend/pages/user/index.tsx
+++ frontend/pages/user/index.tsx
@@ -52,7 +52,7 @@ const UserInfo = () => {
                 <div className="flex items-center justify-between">
                   <span className="text-emerald-400">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z" />
                     </svg>
                   </span>
                 </div>
