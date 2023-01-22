5a892568c9a4a168bc33849302aecde9fa989203
kazuki <kaz.tokyo.tech20@gmail.com>
2023-01-22 16:31:56 +0900

    server side setting for next js

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,22 +1,14 @@
 import Layout from '@/components/layout/Layout'
-import type { AppProps } from 'next/app'
-import { useRouter } from 'next/router'
+import '@/styles/globals.scss'
 import { url } from '@/utils/constants'
-import 'styles/globals.scss'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
+import type { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
   // axios setting
   axios.defaults.withCredentials = true
   axios.defaults.baseURL = url
 
-  const router = useRouter()
-  // tasks/*, user/*, is required to login
-  // jwt token is required to access to these pages
-  if (router.pathname.startsWith('/tasks') || router.pathname.startsWith('/user')) {
-
-  }
   return (
     <Layout>
       <Component {...pageProps} />
